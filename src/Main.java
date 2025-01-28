99d31364147353ed8b119a032aaf5b76ef003d1d
ouYrNmae <148975441+ouYrNmae@users.noreply.github.com>
2025-01-28 14:26:28 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        hellowWorld();
+        helloName("Alex McNeil");
+        helloName("Ren", "7DWY");
+        addingNumbers(5, 6);
+        int total = addingNumbers(4, 5 ,6);
+    }
+
+    static void hellowWorld(){
+        System.out.println("Hello World!");
+    }
+    static void helloName(String a){
+        System.out.println("Hello " + a);
+    }
+    static void helloName(String a, String b){
+        System.out.println("Hello " + a + " and " + b);
+    }
+    static void addingNumbers(int a, int b){
+        System.out.println(a+b);
+    }
+    static int addingNumbers(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    }
+    static void squares(int a){
+        System.out.println(a*a);
     }
 }
